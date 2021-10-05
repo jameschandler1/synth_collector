0a233ee07a1ee5c458bfd78507149f7930f200d8
James Chandler <stillsound@protonmail.com>
2021-10-04 17:36:16 -0700

    create templateview functions

--- synth_collector/main_app/views.py
+++ synth_collector/main_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views import View # <- View class to handle requests
 from django.http import HttpResponse 
+from django.views.generic.base import TemplateView
 # Create your views here.
 
+
+# regular views
 class Home(View):
     
     def get(self, request):
@@ -17,3 +20,15 @@ class Index(View):
 
     def get(self, request):
         return HttpResponse("indexpage")
+
+
+#template views
+
+class Home(TemplateView):
+    template_name = "home.html"
+
+class About(TemplateView):
+    template_name = "about.html"
+
+class Index(TemplateView):
+    template_name = "index.html"
