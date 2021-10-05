0b7d0b62ef407ec5ae272d1ff72f226b38d77a45
James Chandler <stillsound@protonmail.com>
2021-10-04 17:33:13 -0700

    add index, about, home routes

--- synth_collector/main_app/views.py
+++ synth_collector/main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.views import View # <- View class to handle requests
+from django.http import HttpResponse 
 # Create your views here.
+
+class Home(View):
+    
+    def get(self, request):
+        return HttpResponse("homepage")
+
+class About(View):
+
+    def get(self, request):
+        return HttpResponse("aboutpage")
+
+class Index(View):
+
+    def get(self, request):
+        return HttpResponse("indexpage")
